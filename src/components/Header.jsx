949ecd1d76d849db7cdff111570f2c0689619b9c
Orlando Porta <orlando_12368@hotmail.com>
2024-04-06 22:53:36 -0500

    se elimino datos innecesarios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      jhfksdjhfdskñglkfdjgfg
-      <GfdgdfgD>
-        <Dfd>dfg
-            <Dfg><dfn><dfn><datagrid>static getDerivedStateFromProps(nextProps, prevState) {
-                dfnD
-                
-            }
-            </datagrid></dfn></dfn></Dfg>
-        </Dfd>
-      </GfdgdfgD>
+
     </header>
   )
 }
