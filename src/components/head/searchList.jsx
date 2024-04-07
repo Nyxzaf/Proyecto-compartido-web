fcacd9e95e5182a449cba150d9879b13b6711680
Orlando Porta <orlando_12368@hotmail.com>
2024-04-07 18:47:38 -0500

    Se agrego searchLista.jsx

--- src/components/head/searchList.jsx
+++ src/components/head/searchList.jsx
@@ -1,19 +1,49 @@
-import { Autocomplete , Stack, TextField } from '@mui/material';
-const SearchList = () => {
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-    const ListCategory = ["Bebidas" , "Cuidado Personal y salud" , "Limpieza" , "Mascotas" , "Abarrotes", "ElectroHogar", "Frutas y Verduras", "DecoHogar", "Mejoramiento del Hogar"]
+export default function SelectLabels() {
+  const [age, setAge] = React.useState('');
 
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
-
-    return (
-        <Stack width={300} p={1} height={40} bgcolor={'white'} borderRadius="10px" boxShadow={1} direction={"row"} alignItems={"center"}>
-            <Autocomplete
-                sx={{borderRadius:"10px"}}
-                options={ListCategory}
-                renderInput={(e)=> <TextField  {...e} variant='standard' fullWidth={true} />}
-            />  
-        </Stack>
-    );
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <Select
+          value={age}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label' }}
+          MenuProps={{
+            PaperProps: {
+              style: {
+                maxHeight: 300,
+              },
+            },
+          }}
+        >
+          <MenuItem value="">
+            <em>Todas las Categorias</em>
+          </MenuItem>
+          <MenuItem value={20}>Bebidas</MenuItem>
+          <MenuItem value={30}>Cuidado Personal y Salud</MenuItem>
+          <MenuItem value={40}>Limpieza</MenuItem>
+          <MenuItem value={50}>Mascotas</MenuItem>
+          <MenuItem value={60}>Abarrotes</MenuItem>
+          <MenuItem value={70}>Electrohogar</MenuItem>
+          <MenuItem value={80}>Frutas y Verduras</MenuItem>
+          <MenuItem value={90}>Decohogar</MenuItem>
+          <MenuItem value={100}>Mejoramiento del Hogar</MenuItem>
+          <MenuItem value={110}>Congelados</MenuItem>
+          <MenuItem value={120}>Quesos y Fiambres</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
-
-export default SearchList;
