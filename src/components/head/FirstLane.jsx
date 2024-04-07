96e08d72aed258d8004a5577b4173bde9369d5e3
Orlando Porta <orlando_12368@hotmail.com>
2024-04-07 12:36:04 -0500

    se termino FirstLane

--- src/components/head/FirstLane.jsx
+++ src/components/head/FirstLane.jsx
@@ -1,10 +1,11 @@
 import { Grid, Stack } from "@mui/material";
-
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
 
 const FirstLane = () => {
   return (
     <div>
-      <Stack direction="row" alignItems="center" bgcolor="#A93226" pr={3} p={1}  fontFamily="sans-serif" fontSize={14}>
+      <Stack  direction="row" alignItems="center" bgcolor="#A93226" pr={3} p={0}  fontFamily="sans-serif" fontSize={13}>
         <Grid
           container
           alignItems="center"
@@ -12,11 +13,17 @@ const FirstLane = () => {
           ml={2}
           spacing={2}
         >
-          <Grid item>Supermercado</Grid>
-          <Grid item>Electro y Hogar</Grid>
+          <Grid item display="flex" alignItems="center" >
+            <ShoppingBasketIcon sx={{margin:1}} />
+            Supermercado
+            </Grid>
+          <Grid item display="flex" alignItems="center">
+            <DinnerDiningIcon sx={{margin:1}}/>
+            Electro y Hogar
+            </Grid>
         </Grid>
         <Grid container justifyContent="flex-end" spacing={2} pr={1} color="#e3bebf" fontFamily="rubik "  fontSize={14}>
-          <Grid item component=""><a>Agora</a></Grid>
+          <Grid item component="a" color="#e3bebf" href="https://app.agora.pe/">Agora</Grid>
           <Grid item>Financier Oh!</Grid>
           <Grid item>Inkafarma</Grid>
           <Grid item>Mifarma</Grid>
