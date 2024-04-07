e6a7fe0f2da5c78309a727bb56d96082711e14c7
angelo <ancricoma_angelo@hotmail.com>
2024-04-07 18:49:15 -0500

    button

--- src/components/head/searchList.jsx
+++ src/components/head/searchList.jsx
@@ -1,18 +1,18 @@
-import { Autocomplete , Stack, TextField } from '@mui/material';
+import { FormControl, MenuItem, Select } from "@mui/material";
+
+
 const SearchList = () => {
-
-    const ListCategory = ["Bebidas" , "Cuidado Personal y salud" , "Limpieza" , "Mascotas" , "Abarrotes", "ElectroHogar", "Frutas y Verduras", "DecoHogar", "Mejoramiento del Hogar"]
-
 
 
     return (
-        <Stack width={300} p={1} height={40} bgcolor={'white'} borderRadius="10px" boxShadow={1} direction={"row"} alignItems={"center"}>
-            <Autocomplete
-                sx={{borderRadius:"10px"}}
-                options={ListCategory}
-                renderInput={(e)=> <TextField  {...e} variant='standard' fullWidth={true} />}
-            />  
-        </Stack>
+        <FormControl sx={{Width:"140px"}}>
+            <Select displayEmpty value={"categorias"} onChange={(e)=> e.target.value} >
+                <MenuItem value="">
+                    Seleccionar
+                </MenuItem>
+                <MenuItem>aasdasds</MenuItem>
+            </Select>
+        </FormControl>
     );
 }
 
