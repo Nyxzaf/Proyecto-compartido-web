53080a24713021ac3a293883e61a8e0cd4071dfc
angelo <ancricoma_angelo@hotmail.com>
2024-04-07 12:01:47 -0500

    finish top

--- src/components/head/FirstLane.jsx
+++ src/components/head/FirstLane.jsx
@@ -1,9 +1,10 @@
 import { Grid, Stack } from "@mui/material";
 
+
 const FirstLane = () => {
   return (
     <div>
-      <Stack direction="row" alignItems="center" bgcolor="#A93226" pr={3}>
+      <Stack direction="row" alignItems="center" bgcolor="#A93226" pr={3} p={1}  fontFamily="sans-serif" fontSize={14}>
         <Grid
           container
           alignItems="center"
@@ -14,14 +15,15 @@ const FirstLane = () => {
           <Grid item>Supermercado</Grid>
           <Grid item>Electro y Hogar</Grid>
         </Grid>
-        <Grid container justifyContent="flex-end" spacing={3} pr={2}>
-          <Grid item>Hola</Grid>
-          <Grid item>Hola</Grid>
-          <Grid item>Hola</Grid>
-          <Grid item>Hola</Grid>
-          <Grid item>Hola</Grid>
-          <Grid item>Hola</Grid>
-          <Grid item>Hola</Grid>
+        <Grid container justifyContent="flex-end" spacing={2} pr={1} color="#e3bebf" fontFamily="rubik "  fontSize={14}>
+          <Grid item component=""><a>Agora</a></Grid>
+          <Grid item>Financier Oh!</Grid>
+          <Grid item>Inkafarma</Grid>
+          <Grid item>Mifarma</Grid>
+          <Grid item>Oeshle</Grid>
+          <Grid item>Promart</Grid>
+          <Grid item>Real Plaza</Grid>
+          <Grid item>Vivanda</Grid>
         </Grid>
       </Stack>
     </div>
