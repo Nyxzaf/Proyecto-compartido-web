d461cf5f9ccb2ec28f8cea1c56ff5fe9cf1ea35e
Orlando Porta <orlando_12368@hotmail.com>
2024-04-08 16:13:54 -0500

    Se termino FirstContainer

--- src/components/Main/FirstContainer.jsx
+++ src/components/Main/FirstContainer.jsx
@@ -18,76 +18,84 @@ const FirstContainer = () => {
 
   return (
     <Container>
-    <Grid container spacing={5}>
-      <Grid item xs={12} md={6}>
-        <Card sx={{ bgcolor: "#EBEBEB" }}>
-          <CardContent>
-            <Typography variant="h5" gutterBottom style={{ color: "#333" }}>
-              Supermercado
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom style={{ color: "#333" }}>
-              Compra de todo a precios bajos. Entregas el mismo día.
-              <Button variant="contained" sx={{ bgcolor: "#757575", color: "white", mt: 2 }}>
-              Ingresar
-            </Button>
-            </Typography>
-            <Grid container spacing={2}>
-              {supermarketImages.map((imageUrl, index) => (
-                <Grid key={index} item xs={6} md={3}>
-                  <div className="categories__item">
-                    <div className="categories__item--shape categories__item--circular">
-                      <img
-                        className="image__shape image__round"
-                        src={imageUrl}
-                        alt={`Image ${index + 1}`}
-                        style={{ width: "100%" }}
-                      />
+      <Grid container spacing={5}>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ bgcolor: "#EBEBEB",boxShadow: "10", paddingLeft: '20px', paddingRight: '20px', maxWidth: '600px', margin: 'auto' }}>
+            <CardContent>
+              <Typography variant="h5" gutterBottom sx={{ color: "#343333", fontWeight: 'bold', fontSize: '2rem' }}>
+                Supermercado
+              </Typography>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Typography variant="subtitle1" gutterBottom sx={{ color: "#343333", marginRight: '10px', lineHeight: '1.4' }}>
+                  Compra de todo a precios bajos. Entregas el mismo día.
+                </Typography>
+                <Button variant="contained" sx={{ bgcolor: "red", color: "white", borderRadius: "20px" }}>
+                  Ingresar
+                </Button>
+              </div>
+              <Grid container spacing={2}>
+                {supermarketImages.map((imageUrl, index) => (
+                  <Grid key={index} item xs={6} md={3}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                      <div className="categories__item">
+                        <div className="categories__item--shape categories__item--circular" style={{ height: '200px' }}>
+                          <img
+                            className="image__shape image__round"
+                            src={imageUrl}
+                            alt={`Image ${index + 1}`}
+                            style={{ width: "100%", height: "100%" }}
+                          />
+                        </div>
+                        <Typography variant="subtitle2" sx={{ color: "#343333", fontWeight: 'bold', textAlign: 'center' }}>
+                          {index === 0 ? "Carnes, Aves, Pescados" : index === 1 ? "Abarrotes" : index === 2 ? "Lácteos" : "Frutas y Verduras"}
+                        </Typography>
+                      </div>
                     </div>
-                    <Typography variant="subtitle2" style={{ color: "#333" }}>
-                      Carnes, Aves, Pescados
-                    </Typography>
-                  </div>
-                </Grid>
-              ))}
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Card sx={{ bgcolor: "#EBEBEB" }}>
-          <CardContent>
-            <Typography variant="h5" gutterBottom style={{ color: "#333" }}>
-              Electro, hogar y más
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom style={{ color: "#333" }}>
-              Compra de todo a precios bajos. Entregas el mismo día. 
-              <Button variant="contained" sx={{ bgcolor: "#757575", color: "white", mt: 2 }}>
-              Ingresar
+                  </Grid>
+                ))}
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ bgcolor: "#EBEBEB", boxShadow: "10", paddingLeft: '10px', paddingRight: '1px', maxWidth: '700px', margin: 'auto', paddingBottom:"30px" }}>
+            <CardContent style={{ display: 'flex', alignItems: 'center' }}>
+              <div style={{ flex: '1' }}>
+                <Typography variant="h5" gutterBottom sx={{ color: "#343333", fontWeight: 'bold', fontSize: '2rem'  }}>
+                  Electro, hogar y más
+                </Typography>
+                <Typography variant="subtitle1" gutterBottom sx={{ color: "#343333", marginRight: '12px', lineHeight: '1.4' }}>
+                  Lo último en tecnología, electrohogar, deportes, moda, infantil y más. Entregas desde 24 hrs.
+                </Typography>
+              </div>
+              <Button variant="contained" sx={{ bgcolor: "red", color: "white", borderRadius: "20px" }}>
+                Ingresar
               </Button>
-            </Typography>
+            </CardContent>
             <Grid container spacing={2}>
               {electroImages.map((imageUrl, index) => (
                 <Grid key={index} item xs={6} md={3}>
-                  <div className="categories__item">
-                    <div className="categories__item--shape categories__item--circular">
-                      <img
-                        className="image__shape image__round"
-                        src={imageUrl}
-                        alt={`Image ${index + 5}`}
-                        style={{ width: "100%" }}
-                      />
+                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <div className="categories__item">
+                      <div className="categories__item--shape categories__item--circular" style={{ height: '200px' }}>
+                        <img
+                          className="image__shape image__round"
+                          src={imageUrl}
+                          alt={`Image ${index + 5}`}
+                          style={{ width: "100%", height: "100%" }}
+                        />
+                      </div>
+                      <Typography  variant="subtitle2" sx={{ color: "#343333", fontWeight: 'bold', textAlign: 'center' }}>
+                        {index === 0 ? "Televisores" : index === 1 ? "Juguetes" : index === 2 ? "Dormitorio" : "Linea Blanca"}
+                      </Typography>
                     </div>
-                    <Typography variant="subtitle2" style={{ color: "#333" }}>
-                      Carnes, Aves, Pescados
-                    </Typography>
                   </div>
                 </Grid>
               ))}
             </Grid>
-          </CardContent>
-        </Card>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
     </Container>
   );
 };
