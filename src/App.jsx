a6b455d824a816a3cf94ad9e6a20fb77fb924e2d
Orlando Porta <orlando_12368@hotmail.com>
2024-04-07 20:47:05 -0500

    se agrego Body.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/head/Header"
-
+import Body from "./components/Main/Body"
 
 function App() {
   return (
     <div>
       <Header/>
+      <Body/>
     </div>
   )
 }
---
+++ src/components/Main/Body.jsx
@@ -0,0 +1,12 @@
+import { AppBar, Box, Stack } from "@mui/material";
+import Section from "./Section.jsx"
+
+const Body = () => {
+  return (
+      <Box>
+        <Section />
+      </Box>
+  );
+};
+
+export default Body;
