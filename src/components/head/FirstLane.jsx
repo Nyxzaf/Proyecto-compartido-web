85812cce672694c297dff519344cd5029b1dff31
angelo <ancricoma_angelo@hotmail.com>
2024-04-09 17:51:27 -0500

    container first lane

--- src/components/head/FirstLane.jsx
+++ src/components/head/FirstLane.jsx
@@ -4,29 +4,32 @@ import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
 
 const FirstLane = () => {
 
-  const items= ["Agora","Financiera oh!", "Inkafarma","Mifarma","Oeshle","Promart","Real Plaza","Vivanda"]
+  const items= ["Agora","FinancieraOH! ", "Inkafarma","Mifarma","Oeshle","Promart","RealPlaza","Vivanda"]
 
 
   return (
       <Stack flexGrow={1} direction="row" alignItems="center" bgcolor="#922B21" pr={3} p={0}  fontFamily="sans-serif" fontSize={13}>
         <Grid
+          wrap="nowrap"
           container
           alignItems="center"
           ml={2}
           spacing={2}
         >
-          <Grid item component="a" xl={1} href="/#" display="flex"  alignItems="center" sx={{textDecoration:"none", color:"#e3bebf"}} >
+          <Grid item component="a" href="/#" display="flex"  alignItems="center" sx={{textDecoration:"none", color:"#e3bebf"}} >
             <ShoppingBasketIcon sx={{margin:1}} />
             Supermercado
           </Grid>
-          <Grid item component="a" xl={2} href="/#" display="flex" alignItems="center" sx={{textDecoration:"none", color:"#e3bebf"}}>
+          <Grid item component="a"  href="/#" display="flex" alignItems="center" sx={{textDecoration:"none", color:"#e3bebf"}}>
             <DinnerDiningIcon sx={{margin:1}}/>
             Electro y Hogar
           </Grid>
+        </Grid>
+        <Grid container justifyContent="flex-end" xl={1} spacing={2} pr={1} wrap="nowrap" alignItems={"center"} fontFamily="rubik "  fontSize={14}>
           {
             items.map((name,index) =>
           (
-            <Grid key={index} item component="a" color="#e3bebf" href="#" sx={{textDecoration:"none" }} display={{ xs: 'none',md:"block",xl: 'block' }}>
+            <Grid key={index} item component="a" alignItems={"center"}  color="#e3bebf" href="#" sx={{textDecoration:"none" }} display={{ xs: 'none',md:"block",xl: 'block' }}>
               {name}
             </Grid>
           ))
