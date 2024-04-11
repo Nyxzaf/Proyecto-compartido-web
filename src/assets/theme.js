5570e91a2a03367f3417e476b3cfc29d02dfd65d
scloudes <sebascloud2001@gmail.com>
2024-04-11 14:17:44 -0500

    Se movio valores como colores y fuentes a un archivo de configuracion

---
+++ src/assets/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#CB4335",
+      dark: "#922B21",
+    },
+    secondary: {
+      main: "#CB4335",
+    },
+  },
+});
+
+export default theme;
